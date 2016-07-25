8f8cf3d1dde7c48a4d6abc61747750bf4aa75978
ovieu <ovie.u.umukoro@gmail.com>
2016-07-25 16:11:00 -0400

    solved the twenty_sixteen function problem

--- Week01-Mon/lab00/lab00.py
+++ Week01-Mon/lab00/lab00.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 def twenty_sixteen():
     """Come up with the most creative expression that evaluates to 2016,
     using only numbers and the +, *, and - operators.
@@ -5,4 +6,4 @@ def twenty_sixteen():
     >>> twenty_sixteen()
     2016
     """
-    return 2*1000+20-4
+    return (2*1000+20-4)
