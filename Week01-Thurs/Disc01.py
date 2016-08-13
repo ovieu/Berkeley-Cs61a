dc195ec4bbd8ec47809bbd99360d9eb2d730a87d
ovieu <ovie.u.umukoro@gmail.com>
2016-08-12 20:39:52 -0400

    Add Code Solution for Question 1.4

--- Week01-Thurs/Disc01.py
+++ Week01-Thurs/Disc01.py
@@ -13,4 +13,15 @@ def wears_jacket(temp, raining):
 	"""
 	return ((temp < 60) or (raining == True)) == True
 
-
+##1.4 handle_overflow(s1, s2)
+def handle_overflow(s1, s2):
+	if (s1 > 30) and (s2 > 30):
+		print("No Space Left")
+	elif (s1 > 30) and (s2 < 30):
+		space = 30 - s2
+		print("{} spot left in Section 2".format(space))
+	elif (s1 < 30) and (s2 > 30):
+		space = 30 - s1
+		print("{} spot left in Section 1".format(space))
+	else:
+		print("No Overflow")
